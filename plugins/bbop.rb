dbeef7d7a2fb985c753f4c28c4664a08d676f5a0
akouryy <akouryy7@gmail.com>
2015-03-04 23:17:24 +0900

    Simplify source code by using Ruby standard library.
    use map and filter method, perfect matching and %r regexp literal in plugins/killme.rb
    
    remove unnecessary split in plugins/bbop.rb
    
    use any? method in plugins/bot.rb

--- plugins/bbop.rb
+++ plugins/bbop.rb
@@ -1,37 +1,46 @@
 require "levenshtein"
 
+B = "バビブベボ".chars.freeze
 def b
-	"バビブベボ".split(//).sample
+	B.sample
 end
+P = "パピプペポ".chars.freeze
 def p
-	"パピプペポ".split(//).sample
+	P.sample
 end
+D = "ダヂヅデド".chars.freeze
 def d
-	"ダヂヅデド".split(//).sample
+	D.sample
 end
+R = "ラリルレロ".chars.freeze
 def r
-	"ラリルレロ".split(//).sample
+	R.sample
 end
+FIGURE = "☆♪＊".chars.freeze
 def figure
-	"☆♪＊".split(//).sample
+	FIGURE.sample
 end
+ER = "ー～＝".chars.freeze
 def er
-	"ー～＝".split(//).sample
+	ER.sample
 end
+
+BBOP = "ビビッドレッド・オペレーション"
 
 on_event(:tweet) do |obj|
 	case obj.text
 	when /^(?!RT)@#{screen_name}\s+bbop/
 		var = rand(49)
-		case var
-		when 0
-			bbop = "ビビッドレッド・オペレーション"
-		when 1
-			bbop = "ビビッドレッド・オペレーション".split(//).reverse.join
-		else
-			bbop = "#{b}#{b}ッ#{d}#{r}ッ#{d}#{figure}オ#{p}#{r}#{er}ション"
-		end
-		per = (1 - Levenshtein.normalized_distance(bbop,"ビビッドレッド・オペレーション")) * 100
+		bbop =
+			case var
+			when 0
+				BBOP
+			when 1
+				BBOP.reverse
+			else
+				"#{b}#{b}ッ#{d}#{r}ッ#{d}#{figure}オ#{p}#{r}#{er}ション"
+			end
+		per = (1 - Levenshtein.normalized_distance(bbop, BBOP)) * 100
 		text = bbop + "(#{per}%)"
 		mention(obj,text)
 	end
