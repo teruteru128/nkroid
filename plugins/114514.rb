bc0a6659a8831844d3070550017370b8d91debf3
sudosan <cream.stew.007+586@gmail.com>
2015-03-09 22:27:07 +0900

    菅　野　美　穂
    意味不明

---
+++ plugins/114514.rb
@@ -0,0 +1,10 @@
+require "open-uri"
+
+url = 'http://114514.jp/114514.png'
+
+on_event(:tweet) do |obj|
+	if obj.text =~ /^(?!RT)@#{screen_name}\s+114514/
+		@rest.update_profile_image(open(url))
+		mention(obj,"イキスギィ！。\n#{Time.now}")
+	end
+end
