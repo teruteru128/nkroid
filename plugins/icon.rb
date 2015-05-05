b372ad84cd945e83cf4e8ed9e8b771e12f38b12f
nkpoid <nkpoidjp@gmail.com>
2015-05-06 07:20:22 +0900

    はぁ

--- plugins/icon.rb
+++ plugins/icon.rb
@@ -2,42 +2,40 @@
 require "open-uri"
 
 def update_icon(obj, type)
-	if obj.media?
-		uri = obj.media[0].media_uri.to_s
-		open(uri) do|file|
-			pass = temp_io(file)
-			io = File.open(pass)
-			case type
-			when "icon"
-				@rest.update_profile_image(io)
-			when "header"
-				@rest.update_profile_banner(io)
-			else
-				return
-			end
-			mention(obj,"#{type}を#{uri}に変更しました！")
-		end
-	else
-		mention(obj,"画像を添付してください。\n#{Time.now}")
+	if @profile_locked
+		mention(obj,"プロフィールは現在#{@locker}によってロックされています。\n#{time}")
+		return
 	end
+	raise "画像を添付してください。" unless obj.media?
+	uri = obj.media[0].media_uri.to_s
+	file = open(uri)
+	case type
+	when "icon"
+		@rest.update_profile_image(file)
+	when "header"
+		@rest.update_profile_banner(file)
+	else
+		return
+	end
+	mention(obj,"#{type}を#{uri}に変更しました！")
+rescue => e
+	mention(obj,"#{e.message}\n#{Time.now}")
 end
 
 def icon_by_search(obj,q)
-	uri = search_image(q)
-	open(uri) do |io|
-		pass = temp_io(io)
-		if File.size?(pass) < 734003200
-			file = File.open(pass)
-			@rest.update_profile_image(file)
-			mention(obj,"アイコンを#{q}の検索結果に変更しました！\n#{uri}")
-			file.close
-		else
-			mention(obj,"Failed!\n#{Time.now}")
-		end
+	if @profile_locked
+		mention(obj,"プロフィールは現在#{@locker}によってロックされています。\n#{time}")
+		return
 	end
+	uri = search_image(q)
+	file = open(uri)
+	@rest.update_profile_image(file)
+	mention(obj,"アイコンを#{q}の検索結果に変更しました！\n#{uri}")
+rescue => e
+	mention(obj,e.message)
 end
 
-on_event(:tweet) do |obj| 
+on_event(:tweet) do |obj|
 	case obj.text
 	when /^(?!RT)@#{screen_name}\supdate_(icon|header)/
 		update_icon(obj,$1)
