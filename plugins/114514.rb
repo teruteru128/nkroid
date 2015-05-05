cac09bebdbbf7b818ad9ef7a4e60d8c456f6542c
nkpoid <nkpoidjp@gmail.com>
2015-05-06 07:22:30 +0900

    さよなら

--- plugins/114514.rb
+++
@@ -1,8 +0,0 @@
-require "open-uri"
-
-on_event(:tweet) do |obj|
-	if obj.text =~ /^(?!RT)@(#{screen_name})\s+114514/
-		@dirty << obj.user.id
-		mention(obj,"イキスギィ！。\n#{Time.now}")
-	end
-end
--- plugins/introduce.rb
+++
@@ -1,6 +0,0 @@
-on_event(:tweet) do |obj|
-	case obj.text
-	when /^(?!RT)@#{screen_name}\s+introduce/
-		twitter.update("#{rand_keyphrase}(@#{obj.user.screen_name})",:in_reply_to_status_id => obj.id)
-	end
-end
