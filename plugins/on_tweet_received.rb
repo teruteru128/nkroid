e1a9e8cbeaa304e9513d4b32f6fb1591d4d5782f
nkpoid <nkpoidjp@gmail.com>
2015-05-06 07:19:09 +0900

    raiseメッセージ 汚い

--- plugins/on_tweet_received.rb
+++ plugins/on_tweet_received.rb
@@ -2,7 +2,8 @@
 require "termcolor"
 
 on_event(:tweet) do |obj|
-	time=t = Time.at(((obj.id >> 22) + 1288834974657) / 1000.0)
-	puts "Tweet -> @#{obj.user.screen_name}: #{obj.text}\n#{time}\n<Red>Rag:#{Time.now-time}</Red>".termcolor
-	next if obj.text =~ /rt|@/i
+	t = Time.at(((obj.id >> 22) + 1288834974657) / 1000.0)
+	rag = Time.now - t
+	raise "遅すぎィ！" if rag > 1
+	$stderr.puts "Tweet -> @#{obj.user.screen_name}: #{obj.text}\n#{t}\n<Red>Rag:#{rag}</Red>".termcolor
 end
