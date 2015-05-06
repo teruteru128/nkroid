c2063c9d66cc49e6f082825ac66f3908a902c072
akouryy <akouryy7@gmail.com>
2015-05-06 22:25:05 +0900

    SUDO_COMMANDSを用いて記述を簡略化しました。
    ……動作確認? なにそれおいしいの?

--- plugins/super_user.rb
+++ plugins/super_user.rb
@@ -24,7 +24,7 @@ def block id
 	user = @rest.user(id)
 	return "suはブロックできません。" if admin? user.id
 	@rest.block user
-	"#{user.screen_name}さんをブロックしました。"
+	"#{user.screen_name}をブロックしました。"
 rescue => e
 	"#{e.class}\n#{e.message}"
 end
@@ -39,40 +39,46 @@ end
 def r4s id
 	user = @rest.user id
 	@rest.report_spam user
-	"#{user.screen_name}さんをスパム報告しました。"
+	"#{user.screen_name}をスパム報告しました。"
 rescue => e
 	"#{e.class}\n#{e.message}"
 end
 
 @profile_locked = false
 
-def execute_sudo(command,obj)
-	case command
-	when /profile\s+(lock|unlock)/
-		case $1
-		when "lock"
-			@profile_locked = true
-			@locker = obj.user.screen_name
-		when "unlock"
-			@profile_locked = false
+SUDO_COMMANDS = {
+	/^profile\s+(lock|unlock)/ =>
+		proc do |match, obj|
+			@profile_locked = (match[1] == "lock")
+			@locker = obj.user.screen_name if @profile_locked
+			"State of Profile:Lock -> #@profile_locked"
+		end,
+
+	/^block\s+@?(\w+)/   => proc{|m| block m[1]},
+	/^unblock\s+@?(\w+)/ => proc{|m| unblock m[1]},
+	/^register\s+su\s+@?(\w+)/ => proc{|m| add_admin m[1]},
+	/^(?:report_spam|r4s)\s+@?(\w+)/ => proc{|m| r4s m[1]},
+
+	/^stop/ =>
+		proc do
+			mention(obj, "bye.\n#{time}")
+			exit
+		end,
+}.freeze
+
+def execute_sudo(command, obj)
+	SUDO_COMMANDS.each do |regex, func|
+		match = regex.match command
+		if match
+			if message = func.(match, obj)
+				mention(obj, "#{message}\n#{time}")
+			end
+			return
 		end
-		mention(obj,"State of Profile:Lock->#{@profile_locked}")
-	when /^block\s+(.+)/
-		mention(obj,block($1)+"\n#{time}")
-	when /unblock\s+(.+)/
-		mention(obj,unblock($1)+"\n#{time}")
-	when /register\s+su\s+(.+)/
-		mention(obj,add_admin($1))
-	when /(?:report_spam|r4s)\s+(.+)/
-		mention(obj,r4s($1))
-	when "stop"
-		mention(obj,"bye.\n#{time}")
-		exit
-	else
-		mention(obj,"Undefined.(#{command.split("\s")[0]})")
 	end
+	mention(obj, "Undefined. (#{command.split[0]})\n#{time}")
 rescue => e
-	mention(obj,e.class)
+	mention(obj, e.class)
 end
 
 on_event(:tweet) do |obj|
@@ -80,15 +86,15 @@ on_event(:tweet) do |obj|
 		command = $1
 		if admin? obj.user.id
 			@rest.fav obj
-			execute_sudo(command,obj)
+			execute_sudo(command, obj)
 		else
-			mention(obj,"You don't have permission.\n#{time}")
+			mention(obj, "You don't have permission.\n#{time}")
 		end
 	end
 end
 
 on_event(:tweet) do |obj|
 	if obj.text =~ /#{REPLY}list/
-		mention(obj,"現在のねくろいど管理者:\n#{list.join("\n")}")
+		mention(obj, "現在のねくろいど管理者:\n#{list.join("\n")}")
 	end
 end
