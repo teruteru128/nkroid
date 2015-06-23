b730a90792c5af7eb535ab21016af30f03b708d0
nkpoid <nkpoidjp@gmail.com>
2015-06-23 23:29:29 +0900

    ok

---
+++ system/tweet.rb
@@ -0,0 +1,21 @@
+require "cgi"
+
+class Twitter::Tweet
+	alias old_text text
+	def text
+		CGI.unescapeHTML(self.old_text)
+	end
+	
+	def mention?
+		!!self.text =~ /^(?!RT)@nkroid/
+	end
+
+	def reply(text)
+		text.scan(/.{1,#{120}}/m).each do |s|
+			twitter.update("@#{self.user.screen_name} #{s}",:in_reply_to_status_id => self.id)
+		end
+	rescue Twitter::Error
+		$accounts.fallback
+		retry
+	end
+end
