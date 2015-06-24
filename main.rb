ef662597ba5b4d9136cc57de77194af33fcf9497
nkpoid <Koukun0811>
2015-06-24 19:00:55 +0900

    Loggerを使うようにした

--- main.rb
+++ main.rb
@@ -8,14 +8,19 @@ $init_thread = Thread.new{loop{sleep 0.4;$stderr.print "."}}
 require "twitter"
 require "pg"
 require "yaml"
+require "logger"
 require "./accounts"
-require "./log"
 $init_thread.kill;puts
 
 #変数定義
 #桐間紗路ちゃんのおパンツドリップコーヒー
+$console = Logger.new($stdout)
+$console.progname = "nkroid"
+$console.datetime_format = "%Y-%m-%d %H:%M:%S"
+$console.formatter = proc{|severity, datetime, progname, message|
+	"[#{severity} #{datetime}] #{message}\n"
+}
 $accounts = AccountManager.new
-$log = Log.new
 $keys = YAML.load_file($dir+"/data/keys.yml")
 $rest = Twitter::REST::Client.new($keys[0]);$accounts<<$rest #メインアカウント
 $keys[1..-2].each{|key|$accounts<<Twitter::REST::Client.new(key)} #規制用アカウント
@@ -27,26 +32,26 @@ $threads = []
 cores = Dir.glob($dir+"/system/*.rb").sort
 cores.each do |core|
 	core =~ /system\/(.+\.rb)$/
-	$log.send "Load #{$1}"
+	$console.info "Load #{$1}"
 	eval(File.read(core)) end
 plugins = Dir.glob($dir+"/plugins/*.rb").sort
 plugins.each do |plugin|
 	plugin =~ /plugins\/(.+\.rb)$/
-	$log.send "Load #{$1}"
+	$console.info "Load #{$1}"
 	eval(File.read(plugin)) end
 
 $markov = Markov.new
 
 def main
 	$stream.user(:replies => "all") do |obj|
-		if $threads.length > 19
-			$threads[0].kill 
+		$threads << Thread.new{extract_obj obj}
+		if $threads.length > 9
+			$threads[0].kill
 			$threads.slice! 0
 		end
-		$threads << Thread.new{extract_obj obj}
 	end
 rescue => e
-	$log.send_error "#{e.class} #{e.message}"
+	$console.error e
 	main
 end
 
