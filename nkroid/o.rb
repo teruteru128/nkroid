7dfc970888278c518a7774893d1fad9ba881a705
nkpoid <nkpoidjp@gmail.com>
2015-08-15 02:33:57 +0900

    none

--- nkroid/o.rb
+++
@@ -1,6 +0,0 @@
-$dir = File.expand_path(".")
-plugins = Dir.glob($dir+"/plugins/*.rb").sort
-plugins.each do |plugin|
-	plugin =~ /plugins\/(.+\.rb)$/
-	puts plugin if (File.read(plugin)) =~ /現代/
-end
