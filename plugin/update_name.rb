b5b60b9386b6fd147747c7f8509d6c74f78e9cec
nkpoid <nkpoidjp@gmail.com>
2016-10-11 23:42:23 +0900

    random_update_nameを追加

--- plugin/update_name.rb
+++ plugin/update_name.rb
@@ -17,3 +17,15 @@ Command.register "update_name" do |tweet, account|
 
   tweet.reply message, account.rest
 end
+
+Command.register "random_update_name" do |tweet, account|
+  result = localdb.execute("select * from name order by random() limit 1")[0] rescue nil
+  message = if result
+    name, screen_name = *result[1, 2]
+    account("nkroid").rest.update_profile(name: name)
+    "@#{screen_name}さんに付けられた#{name}に名前を変更しました。"
+  else
+    "過去に名前が付けられていません。><"
+  end
+  tweet.reply message, account.rest
+end
