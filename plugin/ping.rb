488fed9f00158068b0ec7f1e3905bfdb7633073e
nkpoid <nkpoidjp@gmail.com>
2016-10-11 23:53:36 +0900

    コマンドにねくろいどの文字が含まれていても反応しないように

--- plugin/ping.rb
+++ plugin/ping.rb
@@ -1,4 +1,5 @@
 Tweet.hook do |tweet, account|
+  next if Command.check tweet
   if tweet.text =~ /ねくろいど|^@nkroid\s+ping/
     tweet.reply "pong(#{Time.now - decodeSnowflake(tweet.id)}s)", account.rest
   end
