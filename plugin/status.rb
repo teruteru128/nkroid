1776997cf69b3c4e6dc4aca00fa2f3752ac1079b
nkpoid <nkpoidjp@gmail.com>
2016-10-12 12:37:31 +0900

    引数が足りなかったのを修正

--- plugin/status.rb
+++ plugin/status.rb
@@ -2,9 +2,9 @@
 @started_time = Time.now
 
 Init.hook do |list, account|
-  account("nkroid").rest.update("nkroid has started up(version: #{@now_revision})")  
+  account("nkroid").rest.update("nkroid has started up(version: #{@now_revision})")
 end
 
 Command.register "status" do |tweet, account|
-  tweet.reply "version: #{@now_revision}\nuptime: #{strfsec(Time.now-@started_time)}"
+  tweet.reply "version: #{@now_revision}\nuptime: #{strfsec(Time.now-@started_time)}", account.rest
 end
